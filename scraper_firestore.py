7bebd1a2136637f09af3ffb9fca39545ad417bb1
danielecasale2000-droid <danielecasale2000@gmail.com>
2026-07-08 12:17:10 +0200

    Create scraper_firestore.py

---
+++ scraper_firestore.py
@@ -0,0 +1,13 @@
+{
+  "type": "service_account",
+  "project_id": "bmw-cx",
+  "private_key_id": "31176bee1974b89ac97fcdb8c3f5116ca36c2fc0",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQDMetzRbHDDwjBQ\nZ/5x49lpUTDq0UjNn4WjdRAdo3NAWRnKCNmml2/4X/rdkR3qXoBfc+y7NJBTF329\nyNz7KEp50dXOdaXLJ3T2qg7kfUnIsTm/JmXVx9ZtM3XEJK6f5T7faiTX/w+OTHAE\nZTQ7JnMrD0cqVzP8OXj2AKqVVTIE/cft64wS5+m6UhpstWRouJi0R13wcPVO446A\nqTHaFyN1k6hDQjylferjLx/+7Y++mHF88m59Y5VsWFGJkK+NSa+ivvWD4QEFUZtA\nQtP0uV2RPpjwfk5s1W6XhM0nKOXk97ARfbknIekmX9lgXdnVPMjqz9dEwncz/n3b\nr9T5RMNPAgMBAAECggEASHHjGCN2VDTRjzs477rNBT4puZgKVieFXmMUIZwOMicf\nNvApxJb0qae/po5KbgWrL6ZPoge+dmHlP78RY75jnINWX75YzgAdtWKMwjENWhhI\nIEwHWlVhx/CqcaKZZ7b9roPZp9kK9txn93avqaE2vnO8P+kDVwI5s9aKmTvE6/Un\nYR0o1iGbaOdAMDFQdogO6hFlY4nGFkEYF8RlhyRwCseCOmaaucKsxVFj0nsRylIM\nMRVAfmjUAY/F4zZNw7f0A6ONy/i4hCVSnxmOLl4sjzuqLuUgs9CaEa6EdVVq/t5I\nlm3DJMmowXnxT6UckkidAkb8mGLSe28N2e4QD95WOQKBgQDymOlFoE9h198Nie+N\npQc37P+BkJj1KxnH8wsyxTq4sfjYDnLYAl7WtzYJ9YoycLzwgxVkPhevCBEQHkn8\nXVKXz8A8fLtkeSYYY7Y+piV2xZehMQLFYHVPR79f8Fw9Nc3DDibnX6mIdRSynqQ9\nSoQcIVneuq150pSLhuCyXieI1QKBgQDXxtoODJsBbGz5a8fTygx/nYB4tHv/TTKi\nhga5VBp3XSDLZnLVQhfOomJ0bVAiEjNTsWvbffYsKA9L4eLJnOZZLTLyDooPHU+R\nJVZVvCq1X/x3L12M8OK7Kg7EMgyA8gINghsfD8FZZGfGm7oHL82vrUTPTNHpkKFE\n6X/URVTtkwKBgC1WloIEq9FFXtJQ7Qwzw12YVUgJlgx7uZ28tmMOqSLLYA5A5QWp\nq7/FDbxevy0eFLF+Tgkl8ZcsksPIZVtctYrZsLudNAha+9ujBvQ+aNIBq4zcdAAf\nPs1KQ2HSsepem83QX0Tug+iUuQGI1Hj/7CC1uu39RV53FHvfAS9UmV7hAoGAbvcV\nVlpHMJE47X/vEMx8ZvhxhCGVDvL9UjVIqfBFeBMNdWynK2LE6X/6z9MTfaIMgULh\nq8v4WDb1vtlYjsRAdaXX6EFv4Fv7ejJQVuDgucu2Avj5zCxo/UWguGbgpUNPqPC6\nz9uDXFxVPf8ByQZdabXaiB0CanH1bYIDOf+5sD8CgYAmWPxcV316/rn0f2MYxVxg\nOy6TFtEW27RKtxijhb1upP99sM8Mly464ah1nCydMbxRBvOs5xVkGnj4rPpdkrw+\ntA1fq0OuVA9Kja2Wi7uvzPZ4y+IXbFRA28LAa7neIBtEpa5ym9J7GudQZbfNOApU\n9U8rUrKvZB1c1ci5SLMYSQ==\n-----END PRIVATE KEY-----\n",
+  "client_email": "firebase-adminsdk-fbsvc@bmw-cx.iam.gserviceaccount.com",
+  "client_id": "104925664583890449993",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-fbsvc%40bmw-cx.iam.gserviceaccount.com",
+  "universe_domain": "googleapis.com"
+}
